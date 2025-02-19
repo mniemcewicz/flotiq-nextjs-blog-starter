f8056f7d41b28eedc92484db9e9dd2713c8fce51
Dalia <dalia.bachmatiuk@codewave.pl>
2025-02-19 15:23:39 +0100

    minor ui changes and add changelog

--- src/app/_components/Navigation/Navigation.tsx
+++ src/app/_components/Navigation/Navigation.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Navigation() {
   return (
-    <nav className="sticky top-0 bg-white w-full z-10 py-2 lg:py-4 border-b">
+    <nav className="sticky top-0 bg-white w-full z-10 py-9 border-b text-center md:text-left">
       <Link
-        className="font-bold text-3xl lg:text-4xl 2xl:text-5xl no-underline hover:text-black"
+        className="font-bold text-4xl no-underline hover:text-black"
         href="/"
       >
         Tech & Threads
